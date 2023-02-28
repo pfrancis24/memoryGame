b55ea31f070ac6861ab4786f0d1fca816e160f22
Peter Francis <pfrancis24@gmail.com>
2023-02-28 13:24:13 -0500

    feat: add score text

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const newGameBtn = document.querySelector('button');
 const gameContainer = document.getElementById('container');
 const gameCards = document.getElementById('cards');
+let yourScoreText = document.getElementById('your-score');
+let bestScoreText = document.getElementById('best-score'); //needs implementing
+
+let score = 0;
+let faceUp = [];
 
 const butterflies = [
   'monarch-a',
@@ -21,58 +26,38 @@ const butterflies = [
   'skipper-b',
 ];
 
-// initial attempt, was going to put images here as well as id/num.
-// const butterflies = [
-//   { imageUrl: 'monarch.jpg', name: 'monarch', num: 1 },
-//   { imageUrl: 'swallowtail.jpg', name: 'swallowtail', num: 2 },
-//   { imageUrl: 'buckeye.jpg', name: 'buckeye', num: 3 },
-//   { imageUrl: 'sulphur.jpg', name: 'sulphur', num: 4 },
-//   { imageUrl: 'nymph.jpg', name: 'nymph', num: 5 },
-//   { imageUrl: 'white.jpg', name: 'white', num: 6 },
-//   { imageUrl: 'fritillary.jpg', name: 'fritillary', num: 7 },
-//   { imageUrl: 'skipper.jpg', name: 'skipper', num: 8 },
-//   { imageUrl: 'monarch.jpg', name: 'monarch', num: 1 },
-//   { imageUrl: 'swallowtail.jpg', name: 'swallowtail', num: 2 },
-//   { imageUrl: 'buckeye.jpg', name: 'buckeye', num: 3 },
-//   { imageUrl: 'sulphur.jpg', name: 'sulphur', num: 4 },
-//   { imageUrl: 'nymph.jpg', name: 'nymph', num: 5 },
-//   { imageUrl: 'white.jpg', name: 'white', num: 6 },
-//   { imageUrl: 'fritillary.jpg', name: 'fritillary', num: 7 },
-//   { imageUrl: 'skipper.jpg', name: 'skipper', num: 8 },
-// ];
-// // verfirying name call will work
-// function butterfliesName(butterflyArray) {
-//   for (butterfly of butterflyArray) {
-//     console.log(butterfly.name);
-//   }
-// }
-
-// butterfliesName(butterflies);
-
+// here is a helper function to shuffle an array
+// it returns the same array with values shuffled
+// it is based on an algorithm called Fisher Yates if you want ot research more
 function shuffle(array) {
   let counter = array.length;
+
+  // While there are elements in the array
   while (counter > 0) {
+    // Pick a random index
     let index = Math.floor(Math.random() * counter);
+
+    // Decrease counter by 1
     counter--;
+
+    // And swap the last element with it
     let temp = array[counter];
     array[counter] = array[index];
     array[index] = temp;
   }
+
   return array;
 }
-
-let shuffledButterflies = shuffle(butterflies);
-// this function loops over the array of butterflies
+// this next function loops over the array of butterflies
 // it creates a new div and gives it a class with the value of the butterfly
 // it also adds an event listener for a click for each card
 
+let shuffledButterflies = shuffle(butterflies);
+// if they match: splice index of out of this array per match so it
+
 function createButterflyGameboard(butterflyArray) {
   for (let butterfly of butterflyArray) {
-    // create a new div
     const newDiv = document.createElement('div');
-    // let butterflyNum = butterflies[butterfly.id];
-    // give it a class attribute for the value we are looping over
-    // newDiv.classList.add(butterfly.num, 'cardCover');
     newDiv.classList.add('cardCover');
     newDiv.setAttribute('id', butterfly);
 
@@ -81,50 +66,77 @@ function createButterflyGameboard(butterflyArray) {
     // append the div to the element with an id of game
     gameCards.append(newDiv);
   }
+  yourScoreText.textContent = 0;
 }
 
-// TODO: Implement this function!
-const faceUp = [];
-
+// next function is to trim the a/b and have cards have same class per pair
 function butterflyName(butterfly) {
   return butterfly.slice(0, -2);
 }
 
+// ***** TODO: DO NOT allow more than two in faceUp, console log to find where *****
+
+// click event function and reveal/cover logic here:
 function handleCardClick(event) {
-  // console.log('you just clicked', event.target);
   //begin flip/match process
   let card = event.target;
-  let cardName = butterflyName(card.getAttribute('id'));
+  let cardName = card.getAttribute('id');
 
-  if (faceUp.length === 0 && card.classList.contains('cardCover')) {
-    card.classList.remove('cardCover');
-    card.classList.add('cardReveal', cardName);
-    console.log(card.classList);
-    faceUp.push(cardName);
-    // console.log(faceUp, 'faceUp[] + 1');
-  } else if (faceUp.length > 0 && card.classList.contains('cardCover')) {
-    card.classList.remove('cardCover');
-    card.classList.add(cardName, 'cardReveal');
-    faceUp.push(cardName);
-    console.log(card.classList);
-    // console.log(faceUp[0], faceUp[1], '<<< cards are revealed');
+  if (faceUp.length < 2) {
+    if (faceUp.length === 0 && card.classList.contains('cardCover')) {
+      card.classList.remove('cardCover');
+      card.classList.add('cardReveal', butterflyName(cardName));
+
+      faceUp.push(cardName);
+      console.log(faceUp.length, 'pairA');
+    } else if (faceUp.length > 0 && card.classList.contains('cardCover')) {
+      card.classList.remove('cardCover');
+      card.classList.add(butterflyName(cardName), 'cardReveal');
+
+      faceUp.push(cardName);
+      console.log(faceUp.length, 'pairB');
+    }
   }
-  return faceUp;
+  if (faceUp.length === 2) {
+    if (!isMatch(faceUp)) {
+      setTimeout(function () {
+        if (faceUp.length === 2) {
+          handleUnmatchedCards(faceUp);
+        }
+        faceUp = [];
+        score++;
+        console.log(score, 'Counter correct?'); // something isnt right here, first unmatched pair doesnt increment score counter
+      }, 1000);
+    } else {
+      faceUp = [];
 
-  // function compareFaceUpCards(faceUp) {
-  //   if (faceUp[0] !== faceUp[1]) {
-  //     faceUp[0].remove('cardReveal');
-  //     faceUp[0].classList.add('cardCover');
-  //     faceUp[1].classList.remove('cardReveal');
-  //     faceUp[1].classList.add('cardCover');
-  //     console.log(faceUp[0], faceUp[1], 'covered?');
-  //     //   faceUp.splice(0);
-  //      // to setup max cards up set length check to 2
-  //     //   console.log('faceUp is empty?', faceUp);
-  //   } //else if(faceUp[0] === faceUp[1])
-  //   //stop timers and keep faceup til end of game
-  // }
+      score++; // this one is fine
+    }
+    yourScoreText.textContent = score;
+    console.log();
+  }
 }
+
+function isMatch(cards) {
+  return butterflyName(cards[0]) === butterflyName(cards[1]);
+}
+
+// function handleMatchedCards(cards) {}    MAYBE delete
+
+function handleUnmatchedCards(cards) {
+  const card1 = document.getElementById(cards[0]);
+  const card2 = document.getElementById(cards[1]);
+  card1.classList.remove('cardReveal', butterflyName(cards[0]));
+  card1.classList.add('cardCover');
+  card2.classList.remove('cardReveal', butterflyName(cards[1]));
+  card2.classList.add('cardCover');
+}
+
+// counter should be for the two click/does it match?
+// if cards match reset counter and leave cards face up
+// if cards dont match, flip back both cards - transition rather than timer
+// set timeout is also for limitting quick clicks (in addition to flip back over)
+// also have if two cards up is 2 do not handle additional clicks
 
 // when the DOM loads
 createButterflyGameboard(shuffledButterflies);
